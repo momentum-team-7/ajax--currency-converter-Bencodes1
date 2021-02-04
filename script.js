d181b08bb920b09e2380d09890e0c50e6f7180d1
Bencodes1 <256.ben@gmail.com>
2021-02-04 09:05:00 -0500

    Still working on interactivity, leaving it here for now

--- script.js
+++ script.js
@@ -34,34 +34,33 @@ const currencies = [
   'MYR'
 ]
 
-// const rootElement = document.querySelector('.container')
-// const currTest = document.querySelector('.currencies')
-// console.log(currTest)
+const rootElement = document.querySelector('.container')
+const currTest = document.querySelector('.currencies')
+console.log(currTest)
 
-  // fetch(urlTest)
-  // .then(response => response.json())
-  // .then(data => {
-  //   console.log('working?', data.rates.AUD)
-  //   const czk2AUD = data.rates.AUD
-  //   console.log(czk2AUD)
-  //   currTest.innerHTML = `<p>${czk2AUD}</p>`
-  // })
+function fillDropdowns(){
+  let dropdownA = document.querySelector("#currency-a-dropdown")
+  let dropdownB = document.querySelector("#currency-b-dropdown")
+  for(let i = 0; i < currencies.length; i++) {
+    let optionA = document.createElement("option")
+    let optionB = document.createElement("option")
+    optionA = currencies[i]
+    optionB = currencies[i]
+    console.log(currencies[i])
+    dropdownA.appendChild(optionA)
+    dropdownB.appendChild(optionB)
+  }
+}
 
+let dropdowns = document.querySelectorAll("select")
+let convertButton = document.querySelector("button")
 
-// Structure of program:
-// 1. Get 2 country codes (placeholders CZK and AUD)
-// 2. Get quantity (placeholder x=2)
-// 3. make a call for country b's rate in country a data
-// 4. multiply country a by x and rate 
-
-// Structure of code: 
-// 1-4, then mess with inputs etc
+fillDropdowns()
 
 let currA = 'CAD'
 let currB = 'HKD'
 let amount = 2.5
 
-convert(currA, currB, amount)
 
 function convert(currA, currB, amount){
   let urlFetch = 'https://api.exchangeratesapi.io/latest?base=' + currA
@@ -75,7 +74,9 @@ function convert(currA, currB, amount){
       console.log('Amount:', amount, " at rate of ", xRate)
       let finalConvert = amount * xRate
       console.log("Total: ", finalConvert)
-      moneySection.innerHTML = `<p>${finalConvert}</p>`
+      // moneySection.innerHTML = `<p>${finalConvert}</p>`
       return finalConvert
   })
 }
+
+convert(currA, currB, amount)
